3a323bc3f6ec04a3c5ac0a81533e2f5020df9433
Ally Deeter <allydeeter6639@gmail.com>
2020-11-11 07:51:47 -0800

    adds work from Tues 11/10

---
+++ week01/3-Wednesday/lectureNotes/lecturePractice.py
@@ -0,0 +1,30 @@
+# school = "Digital School"
+
+# print("Digital Crafts")
+# print("Digital Crafts")
+# print("Digital Crafts")
+# print("Digital Crafts")
+# print("Digital Crafts")
+
+# student1 = "Andrea"
+# student2 = "Zach"
+# student3 = "Michael"
+# student4 = "Rick"
+
+# "Good Morning"
+
+
+# formatting = "{} {} {} {}".format(student1, student2, student3, student4)
+
+# print(formatting)
+
+day = "Wednesday"
+tomorrow = "Thursday"
+
+currentDay = f"Today is {day}"
+currentDay1 = f"Tomorrow is {tomorrow}"
+
+# print(f"Today is {day}")
+
+print (currentDay)
+print (currentDay1)
