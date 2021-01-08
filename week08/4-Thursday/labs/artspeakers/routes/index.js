a9ff7d91db1678bab651744ae9be4d7a20b701cb
Ally Deeter <allydeeter6639@gmail.com>
2021-01-08 07:10:08 -0800

    added class work from 1/7

---
+++ week08/4-Thursday/labs/artspeakers/routes/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+let dataFile = require('../data/data.json');
+
+router.get('/', (req, res) => {
+
+    let pageSpeakers = dataFile.speakers;
+    let pagePhotos = [];
+
+    pageSpeakers.forEach(speakerObj => {
+        pagePhotos = pagePhotos.concat(speakerObj.artwork)
+    })
+
+    console.log(pagePhotos);
+
+    res.render('index', {
+        photos: pagePhotos
+    });
+    
+})
+
+module.exports = router
