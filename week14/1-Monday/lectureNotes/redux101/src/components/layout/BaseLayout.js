aa6096c3fb50c0795758b757be511ab256f4ebad
Ally Deeter <allydeeter6639@gmail.com>
2021-02-15 12:06:04 -0800

    add view count page

---
+++ week14/1-Monday/lectureNotes/redux101/src/components/layout/BaseLayout.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const BaseLayout = (props) => {
+  return (
+    <>
+
+        <Link to="/">Home</Link>
+        <Link to="/count">View Count</Link>
+        <br/>
+        <br/>
+      {props.children}
+    </>
+  )
+}
+
+export default BaseLayout
--- week14/1-Monday/lectureNotes/redux101/src/index.js
+++ week14/1-Monday/lectureNotes/redux101/src/index.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import { Provider } from 'react-redux'
+import {
+  BrowserRouter as Router,
+  Route, Switch
+} from 'react-router-dom';
 import reducer from './reducers/counterReducer'
 import App from './App';
+import ViewCount from './components/ViewCount'
+import BaseLayout from './components/layout/BaseLayout'
+
 
 let store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) //reducer
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <BaseLayout>
+          <Switch>
+            <Route exact path='/' component={App}/>
+            <Route path='/count' component={ViewCount}/>
+            
+          </Switch>
+        </BaseLayout>
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
