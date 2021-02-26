48fa27b18239a917035c91e136f44d382deb550b
Ally Deeter <allydeeter6639@gmail.com>
2021-02-26 14:00:31 -0800

    finished the tokens

--- week15/5-Friday/lectureNotes/auth/server/routes/authentication.js
+++ week15/5-Friday/lectureNotes/auth/server/routes/authentication.js
@@ -1,76 +1,63 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-
-const bcrypt = require("bcryptjs"); //encrypt passwords
-const db = require("../models");
+const bcrypt = require('bcryptjs');
+const db = require('../models');
+// ^allows us to encrypt passwords
 const config = require('../secrets');
-
-router.use(express.urlencoded({ extended: false })); //scrape email and pass from request header
+// ^ our secret file to make jwt
+router.use(express.urlencoded({extended: false}));
+// ^allows us to scrape the email and pw from the req header
 router.use(express.json());
-
-const jwt = require("jwt-simple"); //used to create a jwt
-
-/**
- * This function return a jwt
- */
+const jwt = require('jwt-simple');
+// ^allows us to create a json webtoken 'jwt'
+const passport = require('passport');
+// this is our node passport middleware
+require('../config/passAuth'); //imports all of passport auth stuff
+let requireSignIn = passport.authenticate('local', {session: false})
+let requireAuth = passport.authenticate('jwt', {session: false})
+// have to specify that we're not usin cookie sessions which is the default 
+// this function returns json webtoken from user info
 const token = (user) => {
-    
-    let timestamp = new Date().getTime();  //current time
-
+    let timestamp = new Date().getTime();
+    // gives us the current time
     return jwt.encode({sub: user.id, iat: timestamp}, config.secret)
 }
-
-
-router.get("/", (req, res) => {
-  res.send("hello world");
+router.get('/', requireAuth, (req, res) => {
+    res.send('Hello World')
 });
-
-/**
- * logging in with credentials
- */
-router.post("/signin", (req, res) => {
-  //validate user
-
-  //send token
-  res.send("token");
-});
-
-/**
- * registering a new user in our database and send back a jwt
- */
-router.post("/signup", async (req, res) => {
-  //body-parse to scrape info
-  //email, password
-  let email = req.body.email;
-  //encrypt: bcrypt
-  let password = bcrypt.hashSync(req.body.password, 8);
-
-  //models- store in database
-  try {
-    let records = await db.user.findAll({ where: { email: email } });
-
-    if (records.length === 0) {
-      //add a new record
-
-      let user = await db.user.create({email: email, password: password});
-
-      let jwtToken = token(user); //token returns a jwt
-
-      return res.json({token: jwtToken}); //passing a jwt to client
-
-    } else {
-      //send back an error
-
-      return res.status(422).send({error: 'Email already exists'})
+// SIGNING IN with credentials: insert passport middleware that will either allow them to have a token or divert them somewhere else 
+router.post('/signin', requireSignIn, (req, res) => {
+    // validate user
+    // send back a token 
+    res.json({token: token(req.user)})
+})
+// SIGNING UP registering a new user and send back jwt
+router.post('/signup', async (req, res) => {
+    // register new user in db 
+    // have a body parser to scrape information (email and password)
+    let email = req.body.email;
+    // encrypt: bcrypt to encrypt password
+    let password = bcrypt.hashSync(req.body.password, 8);
+    // models - we need to store the new info in our db 
+    try{
+    let records = await db.user.findAll({where: {email: email}})
+    // ^checking if the username already exists, if this call brings back data we need to send an error
+    if(records.length === 0){
+        // add new record
+        let user = await db.user.create({email: email, password: password});
+        let jwtToken = token(user)
+        // return jwt, pass it back to client
+        return res.json({token: jwtToken})
     }
-  } catch (error) {
-      //send back error, can't access database
-      return res.status(423).send({error: `Can't access database`});
-  }
-
-  //create jwt token
-
-  //send back a token
-});
-
+    else{
+        // send back an error to the front end since the email exists
+        return res.status(422).send({error: 'Email already exists'})
+    }
+    } catch(error){
+    // send back error that there was a problem accessing database
+        return res.status(423).send({error: `Can't access database`});
+    }
+    // create jwt token and pass back with the response 
+    // send back token
+})
 module.exports = router;
